db7c4a125c8fc52b7287a241c2c228e67093ead1
Joey Bronner <joeybronner@gmail.com>
2014-04-02 06:47:43 +0200

    fix first winform Signed-off-by: Joey Bronner <joeybronner@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -14,107 +14,59 @@
 #using <System.Windows.Forms.dll>
 #using <System.dll>
 
-#define APIENTRY    WINAPI
-
+#define APIENTRY WINAPI
 
 using namespace System;
 using namespace System::Windows::Forms;
+using namespace System::ComponentModel;
+using namespace System::Collections;
+//using namespace System::Data;
+//using namespace System::Drawing;
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
-    PWSTR pCmdLine, int nCmdShow)
-{
-	MSG  msg;    
-	HWND hwnd;
-	WNDCLASSW wc;
-
-	wc.style         = CS_HREDRAW | CS_VREDRAW;
-	wc.cbClsExtra    = 0;
-	wc.cbWndExtra    = 0;
-	wc.lpszClassName = L"Window";
-	wc.hInstance     = hInstance;
-	wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
-	wc.lpszMenuName  = NULL;
-	wc.lpfnWndProc   = WndProc;
-	wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-	wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-
-	RegisterClassW(&wc);
-	hwnd = CreateWindowW( wc.lpszClassName, L"Window",
-				WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-				100, 100, 350, 250, NULL, NULL, hInstance, NULL);  
-
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
-
-	while( GetMessage(&msg, NULL, 0, 0))
+public __gc class fenetreAccueil : public System::Windows::Forms::Form
+{  
+	public:
+	fenetreAccueil(void)
 	{
-	DispatchMessage(&msg);
+		InitializeComponent();
 	}
 
+
+	private:
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	System::ComponentModel::Container * components;
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	void InitializeComponent(void)
+	{
+	  this->components = new System::ComponentModel::Container();
+	  //this->Size = System::Drawing::Size(300,300);
+	  this->Text = "Accueil";
+	}  
+};
+
+
+
+//void __stdcall WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,long lpCmdLine,int nCmdShow)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PWSTR pCmdLine, int nCmdShow)
+{
 	/* Instanciation de twitCurl */
 	twitCurl twitterObj;
     std::string tmpStr, tmpStr2;
     std::string replyMsg;
     char tmpBuf[1024];
 
-	/* Console */
-	AllocConsole();
-
-    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
-    int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
-    FILE* hf_out = _fdopen(hCrt, "w");
-    setvbuf(hf_out, NULL, _IONBF, 1);
-    *stdout = *hf_out;
-
-    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
-    hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
-    FILE* hf_in = _fdopen(hCrt, "r");
-    setvbuf(hf_in, NULL, _IONBF, 128);
-    *stdin = *hf_in;
-
-	printf("Demarrage de la console...\n\n");
-	system("PAUSE");
-
-
-  return (int) msg.wParam;
+	//http://msdn.microsoft.com/en-us/library/aa290064(VS.71).aspx
+	//System::Windows::Forms::MessageBox::Show("Hello, Windows Forms");
+	System::Threading::Thread::CurrentThread->ApartmentState = System::Threading::ApartmentState::STA;Application::Run(new fenetreAccueil());
+	return 0;
 }
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, 
-    WPARAM wParam, LPARAM lParam)
-{
-  switch(msg)  
-  {
-      //case WM_CREATE:
-      //{
-      //    CenterWindow(hwnd);
-      //    return 0;
-      //}
-
-	  case WM_DESTROY:
-	  {
-		  PostQuitMessage(0);
-		  return 0;    
-	  }
-  
-  }
-
-  return DefWindowProcW(hwnd, msg, wParam, lParam);
-}
-
-void CenterWindow(HWND hwnd)
-{
-    RECT rc;
-    
-    GetWindowRect(hwnd, &rc) ;
-    
-    SetWindowPos(hwnd, 0, 
-        (GetSystemMetrics(SM_CXSCREEN) - rc.right)/2,
-        (GetSystemMetrics(SM_CYSCREEN) - rc.bottom)/2,
-         0, 0, SWP_NOZORDER|SWP_NOSIZE);
-}
-
 
 /*
 
